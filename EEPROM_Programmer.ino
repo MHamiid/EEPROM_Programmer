e83ff419fc91d692a589ae1393aef8b9a0d8444f
MHamiid <mmh699@outlook.com>
2022-07-09 00:13:56 +0200

    Changed the functions description comment style

--- EEPROM_Programmer.ino
+++ EEPROM_Programmer.ino
@@ -20,11 +20,19 @@
 
 
 
-/* 
-*	Write the 15-bit for the EEPROM address and 1-bit for the EEPROM output enable status to the EEPROM indirectly through the two 8-bit shift registers .
-*	The 16 bits are written serially to two (8-BIT SHIFT REGISTER WITH 8-BIT OUTPUT REGISTER),
-*	with the 16-bit output from both register connected to the 15-bit address lines of the EEPROM [A0-A14] and the output enable pin [~OE] .
-*/
+
+/**
+ * @brief Set the EEPROM address and EEPROM output enable
+ *
+ * Write two bytes serially, the 15-bit for the EEPROM address and 1-bit for the EEPROM output enable status to the EEPROM indirectly through the two 8-bit shift registers .
+ * The two bytes are written serially to two (8-BIT SHIFT REGISTER WITH 8-BIT OUTPUT REGISTER),
+ * with the two bytes (16-bit) output from both register connected to the 15-bit address lines of the EEPROM [A0-A14] and the output enable pin [~OE] .
+ *
+ * @param address 15-bit address
+ * @param EEPROMOutputEnable The 16th bit, set to true to enable EEPROM data output at the specified address, set to false to disable the EEPROM form outputting data   
+ *
+ * @return void
+ */
 void setEEPROMAddressAndOutputEnable(uint16_t address, bool EEPROMOutputEnable)
 {
 
@@ -65,9 +73,14 @@ void setEEPROMAddressAndOutputEnable(uint16_t address, bool EEPROMOutputEnable)
 
 
 
-/*
-*	Write a byte to the EEPROM at the specified address
-*/
+/**
+ * @brief Write a byte to the EEPROM at the specified address
+ *
+ * @param address The address where the byte data would be written in
+ * @param data The byte to be written at the specified address
+ *
+ * @return void
+ */
 void writeEEPROMByte(uint16_t address, byte data)
 {
    /*
@@ -108,9 +121,15 @@ void writeEEPROMByte(uint16_t address, byte data)
 
 
 
-/*
-*	Write the given byte array to the EEPROM starting from the specified address
-*/
+/**
+ * @brief Write the given byte array to the EEPROM starting from the specified address
+ *
+ * @param dataArray Byte array
+ * @param dataArraySize Size of dataArray
+ * @param address The starting address where the dataArray is written
+ *
+ * @return void
+ */
 void writeEEPROM(byte* dataArray, uint16_t dataArraySize, uint16_t address)
 { 
    for(uint16_t index = 0; index < dataArraySize; index++)
@@ -123,9 +142,15 @@ void writeEEPROM(byte* dataArray, uint16_t dataArraySize, uint16_t address)
 
 
 
-/*
-*  Override EEPROM content in the given address range [ startAddress:endAddress ] with the specified value
-*/
+/**
+ * @brief Override EEPROM content in the given address range with the specified value
+ *
+ * @param startAddress Start of the address range
+ * @param endAddress End of the address range
+ * @param value The value to write at the address range
+ *
+ * @return void
+ */
 void eraseEEPROM(uint16_t startAddress, uint16_t endAddress, byte value)
 {
 	for(uint16_t address = startAddress; address <= endAddress; address++)
@@ -137,9 +162,13 @@ void eraseEEPROM(uint16_t startAddress, uint16_t endAddress, byte value)
 
 
 
-/*
-*	Read a byte of data from EEPROM at the specified address
-*/
+/**
+ * @brief Read a byte of data from EEPROM at the specified address
+ *
+ * @param address Address of the byte
+ *
+ * @return The byte at the address
+ */
 byte readEEPROMByte(uint16_t address)
 {
 	for(int dataPin = EEPROM_D0_PIN; dataPin <= EEPROM_D7_PIN; dataPin++)
@@ -174,10 +203,17 @@ byte readEEPROMByte(uint16_t address)
 
 
 
-/*
-*	Print the content of the EEPROM at the specified address range to the serial output .
-*	The data is printed in 16-byte blocks .
-*/
+/**
+ * @brief Serial print EEPROM content at the specified address range
+ *
+ * Assuming that "Serial.begin()" is called before calling this function .
+ * The data is printed in 16-byte blocks .
+ *
+ * @param startAddress Start of the address range
+ * @param endAddress End of the address range
+ *
+ * @return void
+ */
 void serialPrintEEPROMContent(uint16_t startAddress, uint16_t endAddress)
 {
 	// Check if the start and end addresses don't exceed the maximum EEPROM address
@@ -257,9 +293,11 @@ void serialPrintEEPROMContent(uint16_t startAddress, uint16_t endAddress)
 
 
 
-/*
-* 	Initialize and configure the pins to behave as an output 
-*/
+/**
+ * @brief Initialize and configure the pins to behave as an output
+ *
+ * @return void
+ */
 void initPins()
 {
 	
